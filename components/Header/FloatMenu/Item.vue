c37ba611c09f3414e1981fb1f2a0cb98389938b7
Israel Senior <israsenior.dev@gmail.com>
2025-06-27 09:33:34 -0500

    _

--- components/Header/FloatMenu/Item.vue
+++ components/Header/FloatMenu/Item.vue
@@ -5,7 +5,7 @@
         { 'bg-secondary': item.featured },
     ]">
         <div :class="[
-            'flex size-16 flex-none items-center justify-center rounded-lg',
+            'flex size-16 lg:size-20 flex-none items-center justify-center rounded-lg',
             { 'bg-primary-50 group-hover:bg-white': !item.featured },
             { 'bg-primary-50 group-hover:bg-primary-50': item.featured },
         ]">
@@ -14,7 +14,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z" />
                 <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z" />
             </svg>
-            <img v-if="item?.image" :src="item.image" :alt="item?.label" class="size-14 object-contain object-center">
+            <img v-if="item?.image" :src="item.image" :alt="item?.label" class="size-14 lg:size-16 object-contain object-center">
         </div>
         <div class="flex-auto">
             <NuxtLink :to="item.hash" @click.native="emit('closeDropdown', true)" :class="['block font-semibold',
